2754c0ce223d134f460e85aeb7b5450243d4eedb
hp <hp@LAPTOP-DR1B8VEQ>
2022-06-15 12:12:54 +0530

    by sachin s

--- GitHub/src/com/cjc/DemoGit.java
+++ GitHub/src/com/cjc/DemoGit.java
@@ -2,6 +2,11 @@ package com.cjc;
 
 public class DemoGit 
 {
+	public void m1()
+	{
+		System.out.println("Hello this is m1");
+	}
+	
     public static void main(String[] args) 
     {
 		System.out.println("Git Demo");
